44358852574f7c3926721bcf6cd2ffb4c4ae3b77
Pratyushhh-sys <kpratyush23127@gmail.com>
2026-09-04 21:15:00 +0530

    Fix react-leaflet import error by removing non-existent Layers export

--- frontend/src/components/GISMap.jsx
+++ frontend/src/components/GISMap.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Circle, Layers } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet';
 import L from 'leaflet';
 import { MapPin, ShieldAlert, Flame, Building2, Eye, Layers3 } from 'lucide-react';
 
